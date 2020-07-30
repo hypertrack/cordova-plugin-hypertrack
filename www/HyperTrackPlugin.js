3ce6855f91f60b110a0c9d4c225d76262c3d196c
Denys <quux@ukr.net>
2020-07-30 12:02:58 +0300

    Added deviceId getter, metadata and name setters

--- www/HyperTrackPlugin.js
+++ www/HyperTrackPlugin.js
@@ -5,10 +5,31 @@ function HyperTrackPlugin() {
 }
 
 // Initialize SDK //
-HyperTrackPlugin.prototype.initialize = function( publishableKey, success, error ){
+HyperTrackPlugin.prototype.initialize = function(publishableKey, success, error) {
 	console.log("Initializing with key " + publishableKey);
 	exec(success, error, "HyperTrackPlugin", 'initialize', [publishableKey]);
 }
+
+// Get the device Id //
+HyperTrackPlugin.prototype.getDeviceId = function(success, error) {
+	console.log("getDeviceId");
+	exec(success, error, "HyperTrackPlugin", 'getDeviceId', []);
+}
+
+// Set the device name //
+HyperTrackPlugin.prototype.setDeviceName = function(deviceName, success, error) {
+	console.log("setDeviceName " + deviceName);
+	exec(success, error, "HyperTrackPlugin", 'setDeviceName', [deviceName]);
+}
+
+// Set the device metadata //
+HyperTrackPlugin.prototype.setDeviceMetadata = function(deviceMetadata, success, error) {
+	const metadataString = JSON.stringify(deviceMetadata);
+	console.log("setDeviceMetadata " + metadataString);
+	exec(success, error, "HyperTrackPlugin", 'setDeviceMetadata', [metadataString]);
+}
+
+
 
 // FIRE READY //
 exec(function(result){ console.log("HyperTrackPlugin is Ready") }, function(result){ console.log("HyperTrackPlugin Ready ERROR") }, "HyperTrackPlugin",'ready',[]);

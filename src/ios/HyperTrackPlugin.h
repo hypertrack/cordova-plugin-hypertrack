3ba712c52902d3679ec983157108737034d38b25
Dmytro Shapovalov <dshtorm@gmail.com>
2020-09-21 18:47:53 +0200

    iOS cordova plugin part init commit.

---
+++ src/ios/HyperTrackPlugin.h
@@ -0,0 +1,12 @@
+//
+//  HyperTrackPlugin.h
+//  
+//
+//  Created by Dmytro Shapovalov on 21.09.2020.
+//
+
+#ifndef HyperTrackPlugin_h
+#define HyperTrackPlugin_h
+
+
+#endif /* HyperTrackPlugin_h */

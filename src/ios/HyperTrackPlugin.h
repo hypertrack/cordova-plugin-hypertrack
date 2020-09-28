0e0bc38d7713d0ed5f06fafced0f8051011f6b45
Dmytro <dshtorm@gmail.com>
2020-09-28 13:07:29 +0200

    iOS Cordova Plugin (#2)
    * iOS cordova plugin part init commit.
    
    * - bridging header for Cordova;
    - set ios-platform to package.json
    
    * implemented notifications and other bridge methods.
    
    * up pod HyperTrack to 4.5.0 varsion
    
    * Fixed pod version
    
    * Fixed iOS notifications
    
    * Refactoring
    
    * Refactoring
    
    * Removed unused lines
    
    * Refactoring

---
+++ src/ios/HyperTrackPlugin.h
@@ -0,0 +1,17 @@
+#import <Cordova/CDV.h>
+
+@interface HyperTrackPlugin : CDVPlugin
+
+  - (void)initialize:(CDVInvokedUrlCommand *)command;
+  - (void)isRunning:(CDVInvokedUrlCommand *)command;
+  - (void)getDeviceId:(CDVInvokedUrlCommand *)command;
+  - (void)syncDeviceSettings:(CDVInvokedUrlCommand *)command;
+  - (void)setTrackingNotificationProperties:(CDVInvokedUrlCommand *)command;
+  - (void)allowMockLocations:(CDVInvokedUrlCommand *)command;
+  - (void)requestPermissionsIfNecessary:(CDVInvokedUrlCommand *)command;
+  - (void)addGeoTag:(CDVInvokedUrlCommand *)command;
+  - (void)setDeviceMetadata:(CDVInvokedUrlCommand *)command;
+  - (void)setDeviceName:(CDVInvokedUrlCommand *)command;
+  - (void)enableDebugLogging:(CDVInvokedUrlCommand *)command;
+
+@end
--- www/HyperTrackPlugin.js
+++ www/HyperTrackPlugin.js
@@ -1,11 +1,12 @@
-var cordova = require('cordova');
 var exec = require('cordova/exec');
+var platform = window.cordova.platformId;
+var channel = require("cordova/channel");
 
 const sdkHandle = { }
 
-/** 
+/**
  * Get the device Id.
- * 
+ *
  * @param {function(string)} success - callback that recieves the deviceId string.
  * @param {function(error)} errror - error callback.
  */
@@ -14,9 +15,9 @@ sdkHandle.getDeviceId = function(success, error) {
 	exec(success, error, "HyperTrackPlugin", 'getDeviceId', []);
 }
 
-/** 
+/**
  * Set the device name.
- * 
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -25,9 +26,9 @@ sdkHandle.setDeviceName = function(deviceName, success, error) {
 	exec(success, error, "HyperTrackPlugin", 'setDeviceName', [deviceName]);
 }
 
-/** 
- * Set the device metadata 
- * 
+/**
+ * Set the device metadata
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -37,9 +38,9 @@ sdkHandle.setDeviceMetadata = function(deviceMetadata, success, error) {
 	exec(success, error, "HyperTrackPlugin", 'setDeviceMetadata', [metadataString]);
 }
 
-/** 
- * Add geotag 
- * 
+/**
+ * Add geotag
+ *
  * @param {Object} geotagData
  * @param {Object} expectedLocation    keys: latitude, longitude
  * @param {function()} success - success callback.
@@ -52,9 +53,9 @@ sdkHandle.addGeoTag = function(geotagData, expectedLocation, success, error) {
 	exec(success, error, "HyperTrackPlugin", 'addGeoTag', [geodataString, locationString]);
 }
 
-/** 
- * Request permissions if required 
- * 
+/**
+ * Request permissions if required
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -63,9 +64,9 @@ sdkHandle.requestPermissionsIfNecessary = function(success, error) {
 	exec(success, error, "HyperTrackPlugin", 'requestPermissionsIfNecessary', []);
 }
 
-/** 
- * Allow mock locations for testing 
- * 
+/**
+ * Allow mock locations for testing
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -74,9 +75,9 @@ sdkHandle.allowMockLocations = function(success, error) {
 	exec(success, error, "HyperTrackPlugin", 'allowMockLocations', []);
 }
 
-/** 
- * Change foreground service notification properties (Android Only) 
- * 
+/**
+ * Change foreground service notification properties (Android Only)
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -85,9 +86,9 @@ sdkHandle.setTrackingNotificationProperties = function(title, message, success, 
 	exec(success, error, "HyperTrackPlugin", 'setTrackingNotificationProperties', [title, message]);
 }
 
-/** 
- * Sync device's tracking state with the platform 
- * 
+/**
+ * Sync device's tracking state with the platform
+ *
  * @param {function()} success - success callback.
  * @param {function(error)} errror - error callback.
  */
@@ -96,10 +97,10 @@ sdkHandle.setTrackingNotificationProperties = function(title, message, success, 
 	exec(success, error, "HyperTrackPlugin", 'syncDeviceSettings', []);
 }
 
-/** 
- * If tracking service is running 
- * 
- * 
+/**
+ * If tracking service is running
+ *
+ *
  * @param {function(number)} success - receives 1 as a callback agrument if sdk tracking service is running and 0 othewise.
  * @param {function(error)} errror - error callback.
  */
@@ -108,53 +109,165 @@ sdkHandle.isRunning = function(success, error) {
 	exec(success, error, "HyperTrackPlugin", 'isRunning', []);
 }
 
-/** Event handler for when callbacks get (un)registered for the sdk state. */
-sdkHandle.onHasSubscribersChange = function () {
-	// If we just registered the first handler, add tracking state listener.
-	  if (this.channel.numHandlers === 1) {
-		  exec(sdkHandle._status, sdkHandlesdkHandle._error, 'HyperTrackPlugin', 'subscribe', []);
-	  } else if (this.channel.numHandlers === 0) {
-		  exec(null, null, 'HyperTrackPlugin', 'unsubscribe', []);
-	  }
-  };
+/* ------------ */
+/* Internal API */
+/* ------------ */
+
+// Event listeners
 
 /**
- * Callback for the HyperTrack sdk state changes
+ * Event listeners collection.
  *
- * @param {String} state  one of "start", "stop" or tracking errors.
+ * __Supported Platforms__
+ *
+ * -iOS
  */
-sdkHandle._status = function (state) {
+var listeners = {};
 
-	// Poison pill guard for android2cordova callback
-	if (state === "") return
-
-    if (state) {
-        if (sdkHandle._lastEvent !== state.event || sdkHandle._lastError !== state.error) {
-
-            cordova.fireWindowEvent('onHyperTrackStatusChanged', state);
-
-            sdkHandle._lastEvent = state.event;
-            sdkHandle._lastError = state.error;
-        }
-    }
+dispatchEvent = function(event) {
+	if (platform === "ios") {
+		var result = undefined;
+		var functions = listeners[event.type];
+		if (functions) {
+			for (var i = 0; i < functions.length; i++) {
+				result = functions[i](event);
+				if (typeof result != "undefined") {
+					if (!result) return result;
+				}
+			}
+		}
+		return result;
+	} else {
+		console.log("Not implemented on " + platform + ".");
+		return undefined;
+	}
 };
 
-/** Error callback for subscribing to the tracking  state */
-sdkHandle._error = function (e) {
-    console.error('Error attaching listener to HyperTrack SDK: ' + e);
+/**
+ * Event channels.
+ *
+ * __Supported Platforms__
+ *
+ * -Android
+ */
+var channel = require("cordova/channel");
+
+var channels = {
+  onHyperTrackStatusChanged: channel.create("onHyperTrackStatusChanged"),
+  onHyperTrackError: channel.create("onHyperTrackError")
 };
 
+/**
+ * Retrieves total number of handlers for all available channels.
+ *
+ * __Supported Platforms__
+ *
+ * -Android
+ */
+function numberOfHandlers() {
+  return (
+    channels.onHyperTrackStatusChanged.numHandlers +
+    channels.onHyperTrackError.numHandlers
+  );
+}
+
+/**
+ * Notifies about any changes to collection of event handlers.
+ *
+ * __Supported Platforms__
+ *
+ * -Android
+ */
+function onEventSubscribersChanged() {
+  console.log("event subscribers changed");
+  // If we just registered the first handler, make sure native listener is started.
+  if (this.numHandlers === 1 && numberOfHandlers() === 1) {
+    console.log("connecting event channel");
+    exec(
+      function(info) {
+        console.log("Received event", info);
+        channels[info.eventType].fire(info.data);
+      },
+      function() {
+        console.log("Error while receiving event.");
+      },
+      "HyperTrackPlugin",
+      "startEventDispatching",
+      []
+    );
+  } else if (numberOfHandlers() === 0) {
+    console.log("disconnecting event channel");
+    exec(null, null, "HyperTrackPlugin", "stopEventDispatching", []);
+  }
+}
+
+for (var key in channels) {
+  console.log("subscriber listener for " + key);
+  channels[key].onHasSubscribersChange = onEventSubscribersChanged;
+}
 
 var hypertrack = {
-    /** 
+
+		/**
+		 * Subscribes listener for given event type.
+		 *
+		 * __Supported Platforms__
+		 *
+		 * -iOS
+		 * -Android
+		 */
+		addEventListener: function(type, listener) {
+		  if (platform === "ios") {
+		    var existing = listeners[type];
+		    if (!existing) {
+		      existing = [];
+		      listeners[type] = existing;
+		    }
+		    existing.push(listener);
+		  } else if (platform === "android") {
+		    if (type in channels) {
+		      channels[type].subscribe(listener);
+		    }
+		  } else {
+		    console.log("Not implemented on " + platform + ".");
+		  }
+		},
+
+		/**
+		 * Unsubscribes listener for given event type.
+		 *
+		 * __Supported Platforms__
+		 *
+		 * -iOS
+		 * -Android
+		 */
+		removeEventListener: function(type, listener) {
+		  if (platform === "ios") {
+		    var existing = listeners[type];
+		    if (existing) {
+		      var index;
+		      while ((index = existing.indexOf(listener)) != -1) {
+		        existing.splice(index, 1);
+		      }
+		    }
+		  } else if (platform === "android") {
+		    if (type in channels) {
+		      channels[type].unsubscribe(listener);
+		    }
+		  } else {
+		    console.log("Not implemented on " + platform + ".");
+		  }
+		},
+
+    /**
      * Enable debug logging.
-     * 
+     *
      * @param {function()} success success callback
      * @param {function(error)} error error callback
      */
     enableDebugLogging: function(success, error) {
         console.log("HyperTrack:enableDebugLogging");
-        const success_cb = function() { 
+        const success_cb = function() {
             if (typeof(success) == 'function') { success(); }
         }
         const error_cb = function(err) {
@@ -162,22 +275,15 @@ var hypertrack = {
         }
         exec(success_cb, error_cb, "HyperTrackPlugin", 'enableDebugLogging', []);
     },
-    /** 
+    /**
      * Initialize SDK.
-     * 
+     *
      * @param {string} publishableKey - account specific key from dashboard. https://dahsboard.hypertrack.com/setup
      * @param {function(sdkInstance)} success - success callaback that receives sdkInstance as an argument.
      * @param {function(error)} errror - error callback
      */
     initialize: function(publishableKey, success, error) {
         console.log("HyperTrack:Initializing with key " + publishableKey);
-
-        // Create new event handler on the window (returns a channel instance)
-        var channel =  cordova.addWindowEventHandler('onHyperTrackStatusChanged');
-        channel.onHasSubscribersChange = sdkHandle.onHasSubscribersChange;	
-        console.debug("HyperTrack status channel created");
-        sdkHandle.channel = channel;
-
         // wrap callbacks
         const success_cb = function() {
             console.debug("HyperTrack init success cb");
